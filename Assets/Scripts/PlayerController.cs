658e466eccf3cc48f7c14a8d7b953f460f6e47cf
Brighton Pauli <brightp@umich.edu>
2023-03-12 06:28:24 -0400

    Starting to incorporate movement with attacks

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,21 +5,36 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float movement_speed = 2;
+    public float cooldown_time = 0.5f;
+
+    private float cooldown = 0f;
+    private bool facing_right = true;
 
     Rigidbody2D rb;
     Animator animator;
     GameObject playerView;
+    Character playerChar;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         playerView = transform.Find("PlayerView").gameObject;
         animator = playerView.GetComponent<Animator>();
+        playerChar = transform.Find("PlayerObject").gameObject.GetComponent<Character>();
     }
 
     void Update()
     {
-        float horizontal_input = Input.GetAxisRaw("Horizontal");
-        float vertical_input = Input.GetAxisRaw("Vertical");
+        float horizontal_input;
+        float vertical_input;
+
+        if (playerChar.isAttacking) {
+            horizontal_input = 0f;
+            vertical_input = 0f;
+        }
+        else {
+            horizontal_input = Input.GetAxisRaw("Horizontal");
+            vertical_input = Input.GetAxisRaw("Vertical");
+        }
         animator.SetFloat("horizontal_input", horizontal_input);
         animator.SetFloat("vertical_input", vertical_input);
         rb.velocity = new Vector2(horizontal_input, vertical_input) * movement_speed;
@@ -30,16 +45,26 @@ public class PlayerController : MonoBehaviour
             animator.SetBool("Moving", false);
         }
         if (horizontal_input > 0) {
+            facing_right = true;
             playerView.transform.localScale = new Vector3(
                 1,
                 playerView.transform.localScale.y,
                 playerView.transform.localScale.z);
         }
         if (horizontal_input < 0) {
+            facing_right = false;
             playerView.transform.localScale = new Vector3(
                 -1,
                 playerView.transform.localScale.y,
                 playerView.transform.localScale.z);
+        }
+
+        if (cooldown > 0f) {
+            cooldown -= Time.deltaTime;
+        }
+        else if (Input.GetMouseButton(0)) {
+            playerChar.Attack(facing_right);
+            cooldown = cooldown_time;
         }
     }
 }
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,43 +4,102 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    public float damage;
-    public int direction;
-
     public bool playerCreated;
+    public float damage = 20f;
 
-    public GameObject slash;
-
+    private Character actor;
+    private GameObject explosion;
     private SpriteRenderer slashSprite;
 
-    // private Color colorGreen = new Color(100, 255, 255, 255);
-    private Color colorGreen = new Color(0.4f, 1.0f, 1.0f);
-    private Color colorRed = new Color(1.0f, 0.3f, 0.3f);
+    private Color colorNormal = new Color(1.0f, 1.0f, 1.0f);
+    private Color colorStrong = new Color(0.4f, 1.0f, 1.0f);
+    private Color colorFire = new Color(1.0f, 0.3f, 0.3f);
+    private Color colorShield = new Color(0.7f, 0.7f, 0.0f);
+
+    private bool isSword = false;
+    private bool isShield = false;
+    private bool isBomb = false;
+    private bool isVampire = false;
+
+    private bool facingRight = true;
 
     private float time = 0f;
 
     // Start is called before the first frame update
     void Start() {
-        slashSprite = slash.GetComponent<SpriteRenderer>();
-        slashSprite.color = colorGreen;
+        slashSprite = transform.Find("slash_0").gameObject.GetComponent<SpriteRenderer>();
+        explosion = Resources.Load(
+            "Prefabs/Explosion.prefab", typeof(GameObject)) as GameObject;
+        slashSprite.color = colorNormal;
+
+        slashSprite.flipX = !facingRight;
+
+        if (isSword) {
+            slashSprite.color = colorStrong;
+            damage = 50f;
+        }
+
+        if (isShield) {
+            slashSprite.color = colorShield;
+            actor.SetShielded(true);
+        }
     }
 
     // Update is called once per frame
     void Update() {
-        // slashSprite.color = colorGreen;
         time += Time.deltaTime;
         if (time >= 0.5f) {
             Destroy(gameObject);
             Debug.Log("End attack");
+            actor.isAttacking = false;
+
+            // if the player is shielded, remove it here
+            if (isShield) actor.SetShielded(false);
         }
+    }
+
+    public void SetAttributes(Character attacker, bool attackerIsPlayer, bool right,
+                       bool sword, bool shield, bool bomb, bool vampire) {
+        actor = attacker;
+        playerCreated = attackerIsPlayer;
+        actor.isAttacking = true;
+
+        facingRight = right;
+
+        isSword = sword;
+        isShield = shield;
+        isBomb = bomb;
+        isVampire = vampire;
     }
     
     // If this was player created, damage all objects with the "Enemy" tag
     // colliding with this object.
     // If this was NOT player created, damage the player if they collide.
     void OnCollisionEnter(Collision collision) {
-        if (playerCreated && collision.gameObject.tag == "Enemy") {
+        Character victim = collision.gameObject.GetComponent<Character>();
+        if (victim == null) return;
+
+        if (playerCreated && victim.tag == "Enemy") {
             Debug.Log("Collision with enemy.");
+            Attack(victim);
+        }
+
+        else if (!playerCreated && victim.tag == "Player") {
+            Debug.Log("Collision with player.");
+            Attack(victim);
+        }
+    }
+
+    void Attack(Character defender) {
+        defender.Damage(damage);
+
+        if (isVampire) {
+            actor.Heal(damage * 0.3f);
+        }
+
+        if (isBomb) {
+            Instantiate(explosion, transform.position, transform.rotation);
+            isBomb = false; // only explode once per attack
         }
     }
 }
